9e711cb483cedc09d982133b1063b6f645b2f7df
joker1007 <kakyoin.hierophant@gmail.com>
2011-12-04 10:53:57 +0900

    category ships migration

--- db/migrate/003_create_category_ships.rb
+++ db/migrate/003_create_category_ships.rb
@@ -1,8 +1,11 @@
 class CreateCategoryShips < ActiveRecord::Migration
   def self.up
     create_table :category_ships do |t|
-      
+      t.integer :daily_task_id
+      t.integer :category_id
     end
+    add_index :category_ships, :daily_task_id
+    add_index :category_ships, :category_id
   end
 
   def self.down
