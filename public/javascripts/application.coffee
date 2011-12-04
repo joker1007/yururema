a3e793fa79db81b5d2f144aaa61b9acc594648ac
joker1007 <kakyoin.hierophant@gmail.com>
2011-12-04 15:25:47 +0900

    loading effect
    上から下にスライドするように

--- public/javascripts/application.coffee
+++ public/javascripts/application.coffee
@@ -1,16 +1,34 @@
 $(->
   $("#remindButton").click((e) ->
     e.preventDefault()
-    $.ajax {
-      type: "GET"
-      url: "/"
-      dataType: "json"
-      success: (data) ->
-        tasks = _.map(data, (elem)->
-          elem.daily_task
-        )
-        html = $("#taskTemplate").render(tasks)
-        $("#taskList").html(html)
-    }
+    reload_task()
   )
 )
+
+reload_task = () ->
+  $.ajax {
+    type: "GET"
+    url: "/"
+    dataType: "json"
+    success: (data) ->
+      tasks = _.map(data, (elem)->
+        elem.daily_task
+      )
+      rendered = $("#taskTemplate").render(tasks)
+      $("#taskList").animate({
+        opacity: 0.0
+        top: '+=100'
+      }, 200, ->
+        ul = $("<ul>").attr("id", "taskList").css({
+          "opacity": 0.0
+          "position": "relative"
+          "top": "-100px"
+        })
+        ul.html(rendered)
+        $(this).replaceWith(ul)
+        ul.animate({
+          opacity: 1.0
+          top: '+=100'
+        }, 200)
+      )
+  }
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,40 @@
 (function() {
+  var reload_task;
   $(function() {
     return $("#remindButton").click(function(e) {
       e.preventDefault();
-      return $.ajax({
-        type: "GET",
-        url: "/",
-        dataType: "json",
-        success: function(data) {
-          var html, tasks;
-          tasks = _.map(data, function(elem) {
-            return elem.daily_task;
-          });
-          html = $("#taskTemplate").render(tasks);
-          return $("#taskList").html(html);
-        }
-      });
+      return reload_task();
     });
   });
+  reload_task = function() {
+    return $.ajax({
+      type: "GET",
+      url: "/",
+      dataType: "json",
+      success: function(data) {
+        var rendered, tasks;
+        tasks = _.map(data, function(elem) {
+          return elem.daily_task;
+        });
+        rendered = $("#taskTemplate").render(tasks);
+        return $("#taskList").animate({
+          opacity: 0.0,
+          top: '+=100'
+        }, 200, function() {
+          var ul;
+          ul = $("<ul>").attr("id", "taskList").css({
+            "opacity": 0.0,
+            "position": "relative",
+            "top": "-100px"
+          });
+          ul.html(rendered);
+          $(this).replaceWith(ul);
+          return ul.animate({
+            opacity: 1.0,
+            top: '+=100'
+          }, 200);
+        });
+      }
+    });
+  };
 }).call(this);
